a3b4b17b5fcde5d98b5810e28a6b6b51b0702c7b
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2013-01-11 17:11:56 -0600

    Moving to backbone and making combination simulation.

--- js/app.js
+++ js/app.js
@@ -2,27 +2,28 @@
  * Main JS for the Random Picker Thingy
  */
 (function($, w, undefined) {
-  $(document).ready(function() {
-    var query = "SELECT DISTINCT member FROM `swdata`";
-    var dataURL = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=tc_data_viz_meetup_commenters&query=' + encodeURI(query) + '&callback=?';
-    var $container = $('#random-picker-container-thingy');
-    var templateLoading = $('#template-loading').html();
-    var templateSquare = $('#template-square').html();
-    var w = $container.width();
-    var h = $container.height();
-    var colors = ['#3366FF', '#6633FF', '#CC33FF', '#FF33CC', '#33CCFF', '#003DF5', '#002EB8', '#FF3366', '#33FFCC', '#B88A00', '#F5B800', '#FF6633', '#33FF66', '#66FF33', '#CCFF33', '#FFCC33'];
-    var colorsUsed = [];
+  var query = "SELECT DISTINCT member FROM `swdata`";
+  var dataURL = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=tc_data_viz_meetup_commenters&query=' + encodeURI(query) + '&callback=?';
+  var colors = ['#3366FF', '#6633FF', '#CC33FF', '#FF33CC', '#33CCFF', '#003DF5', '#002EB8', '#FF3366', '#33FFCC', '#B88A00', '#F5B800', '#FF6633', '#33FF66', '#66FF33', '#CCFF33', '#FFCC33'];
+  var colorsUsed = [];
+  
+  
+  
+  // Functions to get random things
+  var randomInt = function(min, max) {
+    return Math.floor((Math.random() * ((max + 1) - min)) + min);
+  };
+  var randomArrayValue = function(arr) {
+    return arr[randomInt(0, arr.length - 1)];
+  };
+  
+  // Model for entry
+  var Entry = Backbone.Model.extend({
+    initialize: function() {
+      this.set('color', this.pickColorOrder());
+    },
     
-    // Functions to get random things
-    var randomInt = function(min, max) {
-      return Math.floor((Math.random() * ((max + 1) - min)) + min);
-    };
-    var randomArrayValue = function(arr) {
-      return arr[randomInt(0, arr.length - 1)];
-    };
-    
-    // Function to get color
-    var getColor = function() {
+    pickColorRandom: function() {
       var notUsed = false;
       var color;
       
@@ -39,27 +40,128 @@
       
       colorsUsed.push(color);
       return color;
-    };
+    },
     
-    // Mark as loading
-    $container.html(_.template(templateLoading, {}));
-    
-    // Get data and process
-    $.getJSON(dataURL, function(data) {
-      var total = data.length;
-      var columns = Math.ceil(Math.sqrt(total));
-      var rows = Math.ceil(total / columns);
+    pickColorOrder: function() {
+      var index;
       
-      $container.html('');
-      data = _.map(data, function(d, i) {
-        // Use percentage for width and height.
-        d.width = 100 / columns;
-        d.height = 100 / rows;
-        d.color = getColor();
-        
-        $container.prepend(_.template(templateSquare, d));
-        return d;
+      if (colorsUsed.length === colors.length) {
+        colorsUsed = [];
+      }
+      
+      index = colorsUsed.length;
+      colorsUsed.push(colors[index]);
+      return colors[index];
+    }
+  });
+  
+  // Entry collection
+  var Entries = Backbone.Collection.extend({
+    model: Entry
+  });
+  
+  // Entry view
+  var EntryView = Backbone.View.extend({
+    model: Entry,
+    
+    initialize: function() {
+      this.templates = this.templates || {};
+      this.templates.entry = $('#template-entry').html();
+    },
+    
+    render: function() {
+      var data = this.model.toJSON();
+      data.cid = this.model.cid;
+      $cell = $(_.template(this.templates.entry, data)).hide();
+      this.$el.html($cell);
+      $cell.fadeIn(100, 'swing');
+      return this;
+    }
+  });
+  
+  // Grid view
+  var Grid = Backbone.View.extend({
+    collection: Entries,
+    
+    initialize: function() {
+      this.templates = this.templates || {};
+      this.templates.loading = $('#template-loading').html();
+      this.templates.cell = $('#template-cell').html();
+      
+      this.startLoading();
+      this.getData();
+    },
+    
+    setGrid: function() {
+      this.width = $(this.el).width();
+      this.height = $(this.el).height();
+      this.columns = Math.ceil(Math.sqrt(this.collection.length));
+      this.rows = Math.ceil(this.collection.length / this.columns);
+      this.cellWidth = 100 / this.rows;
+      this.cellHeight = 100 / this.columns;
+    },
+    
+    startLoading: function() {
+      this.$el.html(_.template(this.templates.loading, {}));
+    },
+    
+    getData: function() {
+      var thisView = this;
+      
+      $.getJSON(dataURL, function(data) {
+        thisView.collection = new Entries(data);
+        thisView.$el.html('');
+        thisView.setGrid();
+        thisView.makeGrid();
+        thisView.fillGrid();
       });
+    },
+    
+    makeGrid: function() {
+      var thisView = this;
+      
+      this.collection.each(function(m, i) {
+        thisView.$el.prepend(_.template(thisView.templates.cell, {
+          width: thisView.cellWidth,
+          height: thisView.cellHeight,
+          cid: m.cid,
+          color: m.get('color')
+        }))
+      });
+    },
+    
+    fillGrid: function() {
+      var thisView = this;
+      
+      this.collection.each(function(m, i) {
+        var $cell = thisView.$el.find('div[data-cid=' + m.cid + ']');
+        var found = 0;
+        var limitFound = randomInt(4, 10);
+        $cell.attr('data-found-limit', limitFound);
+        
+        var intervalID = w.setInterval(function() {
+          var guess = thisView.collection.at(randomInt(0, thisView.collection.length - 1));
+          var entry = new EntryView({
+            model: guess,
+            el: $cell
+          }).render();
+          
+          if (guess.cid === m.cid) {
+            found += 1;
+            $cell.attr('data-found', found);
+            if (found == limitFound) {
+              w.clearTimeout(intervalID);
+            }
+          }
+        }, randomInt(300, 400));
+      });
+    }
+  });
+
+  // Kick things off
+  $(document).ready(function() {
+    var gridApp = new Grid({
+      el: '#random-picker-container-thingy'
     });
   });
 })(jQuery, window);
